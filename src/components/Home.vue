69c0e9031d3d69595325188f08ecdfc444af1320
student <student@hubu.edu.cn>
2019-03-12 16:53:21 +0800

    2525

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
         <img slot="icon" src="../assets/movie.png">
       </grid-item>
       <grid-item :label="('ball')">
-        <img slot="icon" src="../assets/fire.png">
+        <img slot="icon" src="../assets/ball.png">
       </grid-item>
       <grid-item :label="('music')">
         <img slot="icon" src="../assets/music.png">
@@ -82,7 +82,27 @@ export default {
   data () {
     return {
       demo06_list: urlList,
-      demo06_index: 0
+      type: '1',
+      list: [{
+        src: 'http://somedomain.somdomain/x.jpg',
+        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: '/component/cell'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题二',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: {
+          path: '/component/radio',
+          replace: false
+        },
+        meta: {
+          source: '来源信息',
+          date: '时间',
+          other: '其他信息'
+        }
+      }]
     }
   }
 }
